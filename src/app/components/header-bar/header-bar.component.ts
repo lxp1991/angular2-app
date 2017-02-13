82904a9a6959a5997e1ebfa8d211e3e5da6334fd
Xiangpeng Li <lxp1991@gmail.com>
2017-02-12 23:02:31 -0800

    add header-bar and left-nav components

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'header-bar',
-    templateUrl: 'header-bar.component.html'
+    templateUrl: 'header-bar.component.html',
+    styleUrls: ['./header-bar.component.css']
 })
 export class HeaderBarComponent implements OnInit {
     constructor() { }
